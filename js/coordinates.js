c257129dadbcb86716a234ed4684848d4a44506e
Job Vranish <job.vranish@gmail.com>
2015-09-09 22:37:41 -0400

    added coordinate converter and time differ

---
+++ js/coordinates.js
@@ -0,0 +1,41 @@
+"use strict";
+
+function CoordinateConverter(playerPos, canvasSize) {
+    this.playerPos = playerPos;
+    this.canvasSize = canvasSize;
+}
+
+CoordinateConverter.prototype.worldToCanvas = function(worldCoord)
+{
+  // world is in units of pixels so just need offset
+  var canvasCenterInWorld = this.playerPos;
+  var canvasCenterInCanvas = new Vector2d(this.canvasSize.x, this.canvasSize.y).scale(0.5);
+  return worldCoord.sub(canvasCenterInWorld).add(canvasCenterInCanvas);
+}
+CoordinateConverter.prototype.canvasToWorld = function(canvasCoord)
+{
+  // world is in units of pixels so just need offset
+  var canvasCenterInWorld = this.playerPos;
+  var canvasCenterInCanvas = new Vector2d(this.canvasSize.x, this.canvasSize.y).scale(0.5);
+  return canvasCoord.sub(canvasCenterInCanvas).add(canvasCenterInWorld);
+}
+
+CoordinateConverter.prototype.worldToTile = function(worldCoord)
+{
+  // tile coords are same as world, but just scaled by 32
+  return worldCoord.scale(1/32);
+}
+CoordinateConverter.prototype.tileToWorld = function(tileCoord)
+{
+  // tile coords are same as world, but just scaled by 32
+  return tileCoord.scale(32);
+}
+
+CoordinateConverter.prototype.tileToCanvas = function(tileCoord)
+{
+  return this.worldToCanvas(this.tileToWorld(tileCoord));
+}
+CoordinateConverter.prototype.canvasToTile = function(canvasCoord)
+{
+  return this.worldToTile(this.canvasToWorld(canvasCoord));
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 "use strict";
-function updateCanvas(doc, canvas) {
-  canvas.width = doc.body.clientWidth;
-  canvas.height = doc.body.clientHeight;
+function updateCanvasSize(doc, canvas) {
+  var canvasSize = new Vector2d(doc.body.clientWidth,
+                                doc.body.clientHeight);
+  canvas.width = canvasSize.x;
+  canvas.height = canvasSize.y;
+  return canvasSize;
 }
 
 function randomMap(width, height) {
@@ -41,6 +44,16 @@ MapVector2d.fromVector2d  = function(v) {
 }
 Vector2d.setupPrototype(MapVector2d);
 
+function TimeTracker() {
+  this.last = window.performance.now();
+}
+TimeTracker.prototype.deltaTime = function() {
+  var now = window.performance.now();
+  var dt = (now - this.last) / 1000;
+  this.last = now;
+  return dt;
+}
+
 function main() {
   var canvas = document.getElementById("viewPort");
   var context = canvas.getContext("2d");
@@ -49,45 +62,10 @@ function main() {
   var map = randomMap(mapSizeTiles.x, mapSizeTiles.y);
 
   var playerPos = new TorusVector2d(0, 0);
-  var LEFT = new TorusVector2d(-1, 0);
-  var RIGHT = new TorusVector2d(1, 0);
-  var UP = new TorusVector2d(0, -1);
-  var DOWN = new TorusVector2d(0, 1);
-
-  function worldToCanvas(worldCoord)
-  {
-    // world is in units of pixels so just need offset
-    var canvasCenterInWorld = playerPos;
-    var canvasCenterInCanvas = new Vector2d(canvas.width, canvas.height).scale(0.5);
-    return worldCoord.sub(canvasCenterInWorld).add(canvasCenterInCanvas);
-  }
-  function canvasToWorld(canvasCoord)
-  {
-    // world is in units of pixels so just need offset
-    var canvasCenterInWorld = playerPos;
-    var canvasCenterInCanvas = new Vector2d(canvas.width, canvas.height).scale(0.5);
-    return canvasCoord.sub(canvasCenterInCanvas).add(canvasCenterInWorld);
-  }
-
-  function worldToTile(worldCoord)
-  {
-    // tile coords are same as world, but just scaled by 32
-    return worldCoord.scale(1/32);
-  }
-  function tileToWorld(tileCoord)
-  {
-    // tile coords are same as world, but just scaled by 32
-    return tileCoord.scale(32);
-  }
-
-  function tileToCanvas(tileCoord)
-  {
-    return worldToCanvas(tileToWorld(tileCoord));
-  }
-  function canvasToTile(canvasCoord)
-  {
-    return worldToTile(canvasToWorld(canvasCoord));
-  }
+  var LEFT = new Vector2d(-1, 0);
+  var RIGHT = new Vector2d(1, 0);
+  var UP = new Vector2d(0, -1);
+  var DOWN = new Vector2d(0, 1);
 
   var image = new Image();
   image.src = './images/warrior.base.152.png';
@@ -109,13 +87,14 @@ function main() {
     1 : grass,
   }
 
-  function render() {
-    updateCanvas(document, canvas);
+
+  function render(dt) {
+    var canvasSize = updateCanvasSize(document, canvas);
 
     context.fillStyle = "#FFFF00";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 0, canvasSize.x, canvasSize.y);
 
-    var dir = new TorusVector2d(0,0);
+    var dir = new Vector2d(0,0);
 
     keyHandler.handleKeys(function(currentlyPressedKeys) {
       for (var key in keyDirections) {
@@ -124,22 +103,32 @@ function main() {
         }
       }
     })
+    var speed = 400;
+    playerPos = playerPos.add(dir.scale(speed * dt));
+    var coordConverter = new CoordinateConverter(playerPos, new Vector2d(canvasSize.x, canvasSize.y));
+    var canvasRect = new Rect(new Vector2d(0, 0), new Vector2d(canvasSize.x, canvasSize.y));
 
-    playerPos = playerPos.add(dir.scale(4));
-
-    var tileGrid = new Rect(new Vector2d(0,0), new Vector2d(canvas.width, canvas.height)).map(canvasToTile).outerCorners();
+    var tileGrid = canvasRect.map(function (v) { return coordConverter.canvasToTile(v); } ).outerCorners();
     tileGrid.eachGridPoint(function(tileCoord) {
       var mapCoord = MapVector2d.fromVector2d(tileCoord);
       var tile = map[mapCoord.x][mapCoord.y];
-      var canvasCoord = tileToCanvas(tileCoord);
+      var canvasCoord = coordConverter.tileToCanvas(tileCoord);
       context.drawImage(tileImages[tile],canvasCoord.x,canvasCoord.y);
     });
 
-    var canvasPlayerPos = worldToCanvas(playerPos);
+    var canvasPlayerPos = coordConverter.worldToCanvas(playerPos);
     context.drawImage(image,canvasPlayerPos.x,canvasPlayerPos.y);
 
-    requestAnimationFrame(render);
-
   }
-  render();
+
+  var timeTracker = new TimeTracker();
+  function frame() {
+    var dt = timeTracker.deltaTime();
+    render(dt);
+    requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
 }
+
+
+
