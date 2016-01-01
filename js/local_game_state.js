b9e31a8850815695aae381b69e84ac71715a2235
David Vranish <dvranish.work@gmail.com>
2016-01-01 06:23:11 -0500

    finished map rendering

---
+++ js/local_game_state.js
@@ -0,0 +1,7 @@
+function LocalGameState(world, glInfo, coordConverter, localPlayer, inputs) {
+  this.world = world;
+  this.glInfo = glInfo;
+  this.coordConverter = coordConverter;
+  this.localPlayer = localPlayer;
+  this.inputs = inputs;
+}
---
+++ js/map/map.js
@@ -0,0 +1,31 @@
+function Map(mapSizeTiles, mapSeed, prng) {
+  // if(typeof(tileMap) == "undefined") {
+  //   tileMap = this.generateRandomMap(new Vector2d(100, 100), 3423523);
+  // }
+
+  this.mapSizeTiles = mapSizeTiles;
+  this.mapSeed = mapSeed;
+  this.prng = prng;
+  this.tileSize = new Vector2d(32, 32);
+
+  //this.tileGrid = this.generateRandomMap(mapSizeTiles, mapSeed);
+  this.tileGrid = mapFile;
+  //debug();
+  //this.glInfo = this.generateMapGlCoords();
+}
+
+Map.prototype.generateRandomMap = function(size, seed) {
+  var map = [];
+  var prng = new ParkMillerRNG(seed);
+  for (var x = 0; x < size.x; x++) {
+    map[x] = [];
+    for (var y = 0; y < size.y; y++) {
+      map[x][y] = [];
+      map[x][y][0] = prng.nextInt(0, 255);
+      map[x][y][1] = prng.nextInt(0, 255);
+      map[x][y][2] = prng.nextInt(0, 255);
+    }
+  }
+  return map;
+}
+
